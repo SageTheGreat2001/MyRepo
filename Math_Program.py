a139e404d1b1468f0f0275da9ffa87cecee08ef9
SageTheGreat2001 <jgetts3@ivytech.edu>
2021-08-19 13:57:53 -0400

    Adding Hello World Program

---
+++ Math_Program.py
@@ -0,0 +1,10 @@
+first_num = int(input('Enter in your first number: '))
+second_num = int(input('Enter in your second number: '))
+sum_one = first_num + second_num
+diff = first_num - second_num
+product = first_num * second_num
+divide = first_num / second_num
+print('the sum is: ' + str(sum_one))
+print('the difference is: ' + str(diff))
+print('The product is: ' + str(product))
+print('The quotient is: ' + str(divide))
